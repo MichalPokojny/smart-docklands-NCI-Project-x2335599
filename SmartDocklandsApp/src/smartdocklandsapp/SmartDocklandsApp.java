9a23b7a3a3c01ba50125c3eda083565fed289c6e
MichalPokojny <108884402+MichalPokojny@users.noreply.github.com>
2026-03-16 15:29:29 +0000

    Initial project setup

---
+++ SmartDocklandsApp/src/smartdocklandsapp/SmartDocklandsApp.java
@@ -0,0 +1,16 @@
+package smartdocklandsapp;
+
+/**
+ *
+ * @author Michal Pokojny - x23355999
+ */
+public class SmartDocklandsApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
